27a8c9a805a8421db45f82c92e0929054ecdb889
David <david_javier.torres@uao.edu.co>
2026-04-21 00:37:07 -0500

    MovieController listo

--- El_Telon_BackEnd/src/main/java/com/andres/proyectos/el_telon/movie/MovieController.java
+++ El_Telon_BackEnd/src/main/java/com/andres/proyectos/el_telon/movie/MovieController.java
@@ -1,5 +1,23 @@
 package com.andres.proyectos.el_telon.movie;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/movies")
+@RequiredArgsConstructor
 public class MovieController {
-    
+
+    private final MovieService movieService;
+
+    @GetMapping("/available")
+    public ResponseEntity<List<MovieResponse>> getAvailableMovies() {
+        return ResponseEntity.ok(movieService.getAvailableMovies());
+    }
 }
