48f86606b5ccd261c951a255f2f0119d025b1e98
David <david_javier.torres@uao.edu.co>
2026-04-21 00:23:16 -0500

    amplie entidad Movie con categoria y estado activo

--- El_Telon_BackEnd/src/main/java/com/andres/proyectos/el_telon/movie/Movie.java
+++ El_Telon_BackEnd/src/main/java/com/andres/proyectos/el_telon/movie/Movie.java
@@ -1,9 +1,18 @@
 package com.andres.proyectos.el_telon.movie;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Builder
@@ -11,29 +20,44 @@ import java.sql.Date;
 @AllArgsConstructor
 @Table(name = "peliculas")
 public class Movie {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
     private Long id;
 
     @Getter
     @Setter
+    @Column(nullable = false)
     private String nombre;
 
     @Getter
     @Setter
+    @Column(nullable = false, length = 500)
     private String descripcion;
 
     @Getter
     @Setter
-    @Column(unique = true, name = "imagen_url")
+    @Column(nullable = false, unique = true, name = "imagen_url")
     private String imagen;
 
     @Getter
     @Setter
-    @Column(name = "fecha_estreno")
+    @Column(nullable = false)
+    private String categoria;
+
+    @Getter
+    @Setter
+    @Column(nullable = false, name = "fecha_estreno")
     private Date fechaEstreno;
 
     @Getter
     @Setter
+    @Column(nullable = false)
     private int duracion;
+
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private boolean activo;
 }
