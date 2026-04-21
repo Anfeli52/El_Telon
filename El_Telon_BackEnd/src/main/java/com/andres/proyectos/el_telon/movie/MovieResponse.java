15d3f5245f0b0d5daf83ad839c64a56c445af14f
David <david_javier.torres@uao.edu.co>
2026-04-21 00:30:37 -0500

    Creacion de dto de respuesta para cartelera

---
+++ El_Telon_BackEnd/src/main/java/com/andres/proyectos/el_telon/movie/MovieResponse.java
@@ -0,0 +1,18 @@
+package com.andres.proyectos.el_telon.movie;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.sql.Date;
+
+@Getter
+@Builder
+public class MovieResponse {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private String imagen;
+    private String categoria;
+    private Date fechaEstreno;
+    private int duracion;
+}
