5f2b18b923e29933e14732c63f91e54f11198b57
David <david_javier.torres@uao.edu.co>
2026-04-28 23:14:31 -0500

    feat(ui): ampliar tipado de peliculas para la vista de detalle

--- El_Telon_FrontEnd/src/types/Movie.ts
+++ El_Telon_FrontEnd/src/types/Movie.ts
@@ -7,3 +7,26 @@ export interface Movie {
     fechaEstreno: string;
     duracion: number;
 }
+
+export interface MovieDateOption {
+    id: string;
+    weekDay: string;
+    dayLabel: string;
+}
+
+export interface MovieShowtimeGroup {
+    theater: string;
+    format: string;
+    language: string;
+    times: string[];
+}
+
+export interface MovieDetail extends Movie {
+    tituloOriginal: string;
+    clasificacion: string;
+    director: string;
+    reparto: string;
+    poster: string;
+    dates: MovieDateOption[];
+    schedules: Record<string, MovieShowtimeGroup[]>;
+}
